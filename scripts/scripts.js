6c59f99bd94d316d2c74b4720bf7497f8b8d6804
Noah <nocarm100@gmail.com>
2022-11-08 15:14:56 -0800

    do/while

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+// for (let i = 0; i <1000000; i++)
+// {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 10 ; i++)
+// {
+//     console.log(2 **i);
+// }
+let input;
+do {
+input = prompt ("what is your name?");
+//prompt always returns string
+//convert input to number
+console.log(Number(input));
+} while (!isNaN(Number(input)));
+
+alert(`Hello ${input}`)
+
+
