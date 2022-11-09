31d9af88d6aab4b9f9c06ef65b28399565c83411
Noah <nocarm100@gmail.com>
2022-11-08 16:13:16 -0800

    student activity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,21 +7,60 @@
 // {
 //     console.log(2 **i);
 // }
-let input;
-do {
-input = prompt ("what is your name?");
-//prompt always returns string
-//convert input to number
-console.log(Number(input));
-} while (!isNaN(Number(input)));
+// let input;
+// do {
+// input = prompt ("what is your name?");
+// prompt always returns string
+// convert input to number
+// console.log(Number(input));
+// } while (!isNaN(Number(input)));
 
-alert(`Hello ${input}`)
+// alert(`Hello ${input}`)
 
-let fruits = ['Apple','Banana'];
-fruits.push("Orange");
-console.log(fruits);
+// let fruits = ['Apple','Banana'];
+// fruits.push("Orange");
+// console.log(fruits);
 
-fruits.shift();
-console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// const person = {
+//     name:['Bob', 'Smith'],
+//     age:32,
+//     gender:'male',
+//     hobbies :['music', 'skiing']
+// }
 
+// console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`)
 
+// for (let key in person)
+// {
+//     console.log(key);
+//     console.log(person[key])
+// }
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3]; 
+
+for (let student of students)
+{
+    if (student.program === "CIT" && student ["GPA"] > 3){
+        counter++;
+        console.log(student);
+    }
+}
+console.log(counter);
